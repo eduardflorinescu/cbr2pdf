3aa0964b9454a0558d0f22c9f8a6893c0228baa6
Eduard Florinescu <georgeflorinescu@gmail.com>
2019-02-05 11:25:27 +0200

    use 7z instead

--- cbr2pdf.sh
+++ cbr2pdf.sh
@@ -2,9 +2,9 @@
 set -xev
 ORIGINAL_FOLDER=`pwd` 
 JPEGS=`mktemp -d`
-unrar e "$1" "$JPEGS"
+cp "$1" "$JPEGS"
 cd "$JPEGS"
+7z e "$1" 
 ls -1 ./*jpg | xargs -L1 -I {} img2pdf {} -o {}.pdf
 pdftk *.pdf cat output combined.pdf
 cp "$JPEGS/combined.pdf" "$ORIGINAL_FOLDER/$1.pdf"
-
