c6b8df8f56818cb7f0e2ed5ea321e1604437b6d8
Eduard Florinescu <georgeflorinescu@gmail.com>
2019-06-13 13:36:12 +0300

    added fix for subfolders cannot find bug

--- cbr2pdf.sh
+++ cbr2pdf.sh
@@ -3,8 +3,8 @@ set -xev
 ORIGINAL_FOLDER=`pwd` 
 JPEGS=`mktemp -d`
 cp "$1" "$JPEGS"
-cd "$JPEGS"
-7z e "$1" 
+7z e "$1" -o$ORIGINAL_FOLDER
+cd "$JPEGS" 
 ls -1 ./*jpg | xargs -L1 -I {} img2pdf {} -o {}.pdf
 pdftk *.pdf cat output combined.pdf
 cp "$JPEGS/combined.pdf" "$ORIGINAL_FOLDER/$1.pdf"
